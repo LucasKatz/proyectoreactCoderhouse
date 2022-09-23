4eb5d760351ee28edddf2eeb94a60ead4344a574
Lucas Katz <l.katz92@gmail.com>
2022-09-23 10:54:06 -0300

    Primeros pasos del NavBar

---
+++ src/Componentes/CartWidget/CartiWidget.jsx
@@ -0,0 +1,1 @@
+import cartWidget from "../Assets"
--- src/Componentes/NavBar/NavBar.js
+++ src/Componentes/NavBar/NavBar.js
@@ -1,40 +1,32 @@
 import './NavBar.css'
+import '../NavBar/'
 
 const NavBAr = () => {
     return (
-        <nav>
-            <div className="container-fluid">
-
-                //insertar logo//
-
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0"/>
-
-                    <li className="nav-item">
-                        <a className="nav-link active boton"  aria-current="page">Inicio</a>
-                    </li>
-
-                    <li className="nav-item">
-                        <a className="nav-link active boton" aria-current="page" >Novedades</a>
-                    </li>
-
-                    <li className="nav-item">
-                        <a className="nav-link active boton" aria-current="page">Catalogo</a>
-                    </li>
-
-                    <li className="nav-item">
-                        <a className="nav-link active boton"  aria-current="page">Contacto</a>
-                    </li>
-
-                </div>
-
-                </div>
-
-        </nav>
+        <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <img src={logo} />
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Novedades</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Catalogo</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Contacto</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
     
 }
