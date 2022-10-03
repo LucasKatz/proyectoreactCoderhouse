2d7d0fad5dfaee0df59fd7066059c67db204ee26
Lucas Katz <l.katz92@gmail.com>
2022-10-03 10:38:41 -0300

    Incorporando contenido clase 6

---
+++ src/Componentes/asyncMock.js
@@ -0,0 +1,64 @@
+const products =  [
+    { 
+        id: '1', 
+        name: 'Adel', 
+        price: 1000, 
+        category: 'lampara', 
+        img:'', 
+        stock: 25, 
+        description:'---'
+    },
+    { 
+        id: '2',
+        name: 'Berilo', 
+        price: 800, 
+        category: 'lampara', 
+        img:'', 
+        stock: 16, 
+        description:'---'
+    },
+    {
+        id: '3', 
+        name: 'Double Sh', 
+        price: 1200, 
+        category: 'lampara', 
+        img:'', 
+        stock: 10, 
+        description:'---'
+},
+{ 
+        id: '4', 
+        name: 'Lampara Hat', 
+        price: 1200, 
+        category: 'tablet', 
+        img:'', 
+        stock: 10, 
+        description:'---'
+},
+{ 
+        id: '5', 
+        name: 'Lampara Tokio', 
+        price: 1200, 
+        category: 'tablet', 
+        img:'', 
+        stock: 10, 
+        description:'---'
+},
+{
+        id: '6', 
+        name: 'Lampara Tuel', 
+        price: 1200, 
+        category: 'tablet', 
+        img:'', 
+        stock: 10, 
+        description:'---'
+}
+]
+
+export const getProducts = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(products)
+        }, 2000)
+    })
+}
