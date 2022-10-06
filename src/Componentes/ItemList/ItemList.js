1d0117cae7b9de98ea0660aba005819d80ebdb4f
Lucas Katz <l.katz92@gmail.com>
2022-10-06 12:40:36 -0300

    Creando cards

--- src/Componentes/ItemList/ItemList.js
+++ src/Componentes/ItemList/ItemList.js
@@ -1,13 +1,35 @@
 import Item from "../Item/item"
 import '../ItemList/ItemList.css'
+import Counter from '../Counter/Counter'
 
-const ItemList = ({ products }) => {
-
+const ItemList = ({products}) => {
     return (
-        <div className ="cardConteiner">
-            { products.map(prod => <Item className="cards" key={prod.id} img={prod.img} name={prod.name} category={prod.category} price={prod.price}/>)}
+        
+        <div className="contenedorLista">
+          {products.map((prod) => {
+            return ( 
+              <div className="cardContainer" key={prod.id}>
+                <div className="imageCard">
+                  <img src={prod.img} alt="" />
+                </div>
+                <div className="detailsCard">
+                  <h5>{prod.name}</h5>
+                </div>
+                <div className="category">
+                  <h3>{prod.category}</h3>
+                </div>
+                <div className="price">
+                    <h5>{prod.price}</h5>
+                </div>
+                <div className="counter">
+                    <Counter/>
+                </div>
+            </div>
+            );
+          })}
         </div>
-    )
-}
+      );
+    };
+
 
 export default ItemList
