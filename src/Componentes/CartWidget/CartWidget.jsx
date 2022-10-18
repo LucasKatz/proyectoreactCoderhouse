ba27c0cd8533292e1a21febf7e843afc7c895fd1
Lucas Katz <l.katz92@gmail.com>
2022-10-18 15:57:19 -0300

    Terminando carrito intento 1

--- src/Componentes/CartWidget/CartWidget.jsx
+++ src/Componentes/CartWidget/CartWidget.jsx
@@ -1,13 +1,21 @@
 import Carrito from "../Assets/images.png"
 import  "../CartWidget/CartWidget.css"
+import { useCartContext } from "../../CartContext/CartContext";
+import ItemCart from "../ItemCart/ItemCart";
 
-export default function CartWidget() {
+
+const CartWidget = () => {
+  const {cart,totalPrice, totalProducts} = useCartContext ();
     return (
       <div className="container-widget">
         <div className="widget">
           <img id="carrito" src={Carrito} alt="" />
-          <span id="contador">0</span>
+          <span id="contador">{totalProducts}</span>
+          {cart.map(product => <ItemCart key={product.id} product = {product}/>)}
+          <p>Total a Pagar: ${totalPrice}</p>
         </div>
       </div>
     );
   }
+
+  export default CartWidget
--- src/Componentes/ItemCart/ItemCart.js
+++ src/Componentes/ItemCart/ItemCart.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import '../ItemCart/ItemCart.css'
 import '../AsyncMock'
+import { useCartContext } from '../../CartContext/CartContext'
 
 
-const ItemCart = ({ img, name, category, price, description }) => {
+
+const ItemCart = ({ img, name, price, quantity}) => {
+    const {removeProduct} = useCartContext ();
 
 return (    
     <div className='containerCart'>
         <img src={img} alt={name}/>
         <h1 className="name">{name}</h1>
-        <h3 className="category">{category}</h3>
-        <p className="price">${price}</p>
-        <p className= "description ">{description}</p>
+        <p className="price">Precio U: ${price}</p>
+        <p className="SubTotal">Subtotal: ${quantity * price}</p>
+        <button onClick={()=>removeProduct(product.id)}> Eliminar</button>
+
     </div>
 )
 }
