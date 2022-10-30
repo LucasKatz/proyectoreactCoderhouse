955f3845b33f58b527a08de5bcd46bc65566220c
Lucas Katz <l.katz92@gmail.com>
2022-10-30 13:33:40 -0300

    agregue .env

--- src/Service/Firebase/index.js
+++ src/Service/Firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCeKQRc_NC8O7yqyfxvBDnQ0rbqunRyPTo",
-    authDomain: "proyectoreactcoderhouse-84537.firebaseapp.com",
-    projectId: "proyectoreactcoderhouse-84537",
-    storageBucket: "proyectoreactcoderhouse-84537.appspot.com",
-    messagingSenderId: "623781412816",
-    appId: "1:623781412816:web:cfb366020840c8ec8b03d5"
+    apiKey: process.env.REACT_APP_apiKey ,
+    authDomain:process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 
